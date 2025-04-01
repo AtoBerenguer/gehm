c1710a02902f7d4ebc458fac6599fa50881c7834
Ato <ato19942@gmail.com>
2025-04-01 20:18:53 +0200

    Añadir logica para crear las tareas

--- src/components/task/modalCreateTask.vue
+++ src/components/task/modalCreateTask.vue
@@ -1,72 +1,90 @@
 <template>
-    <div class="modal">
-      <div class="modal-content">
-        <h2>Crear tarea</h2>
-          
-        
-        
-            <label for="inventario_id">Equipo:</label>
-            <input type="text" placeholder="Ingrese el número de equipo">
-                <hr>
+  <div class="modal">
+    <div class="modal-content">
+      <h2>Crear tarea</h2>
 
+      <label for="inventario_id">Equipo:</label>
+      <input v-model="nuevaTarea.inventario_id" type="text" placeholder="Ingrese el número de equipo">
+      <hr>
 
+      <label for="descripcion">Avería:</label>
+      <input v-model="nuevaTarea.descripcion" type="text" placeholder="Ingrese la descripción de la avería">
+      <hr>
 
+      <label for="tecnico_id">Técnico</label>
+      <select v-model="nuevaTarea.usuario_id">
+        <option v-for="tecnico in tecnicos" :key="tecnico.id_usuario" :value="tecnico.id_usuario">
+          {{ tecnico.id_usuario }} - {{ tecnico.nombre }} {{ tecnico.apellidos }}
+        </option>
+      </select>
 
-            <label for="descripcion">Avería:</label>
-            <input type="text" placeholder="Ingrese el la descripción de la avería">
-                <hr>
-
-
-
-            <label for="tecnico_id">Tecnico</label>
-            <select v-model="tecnicos" name="" id="">
-              <option v-for="tecnico in tecnicos" :key="tecnico.id_usuario" :value="tecnico.id_usuario">
-               {{ tecnico.id_usuario }} - {{ tecnico.nombre }} {{ tecnico.apellidos }}
-              </option>
-            </select>
-
-
-
-
-
-
-
-          <div class="modal-buttons">
-            <button @click="guardarEquipo">Guardar</button>
-            <button @click="$emit('cerrarModalTask')">Cancelar</button>
-          </div>
-
-
+      <div class="modal-buttons">
+        <button @click="crearTarea">Guardar</button>
+        <button @click="$emit('cerrarModalTask')">Cancelar</button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    props: ["mostrarModal"],
-    data() {
-      return {
-        tecnicos: [],
+  </div>
+</template>
 
-      };
-    },
-    mounted() {
-      this.cargarTecnicos();
-    },
-    methods: {
-      async cargarTecnicos() {
+<script>
+import axios from "axios";
+
+export default {
+  props: ["mostrarModal"],
+  data() {
+    return {
+      tecnicos: [],
+      nuevaTarea: {
+        inventario_id: "",
+        descripcion: "",
+        usuario_id: ""
+      }
+    };
+  },
+  mounted() {
+    this.cargarTecnicos();
+  },
+  methods: {
+    async cargarTecnicos() {
+      try {
         const response = await axios.get("http://localhost/BDD-MedicalEquipment/controller/users/getAll_users.php");
         this.tecnicos = response.data;
-        console.log(this.tecnicos);
-        
+      } catch (error) {
+        console.error("Error cargando técnicos:", error);
       }
     },
-   
-      
-  };
-  </script>
+    async crearTarea() {
+      const { inventario_id, descripcion, usuario_id } = this.nuevaTarea;
+      if (!inventario_id || !descripcion || !usuario_id) {
+        alert("Todos los campos son obligatorios");
+        return;
+      }
+
+      try {
+        let formData = new FormData();
+        formData.append("inventario_id", inventario_id);
+        formData.append("descripcion", descripcion);
+        formData.append("usuario_id", usuario_id);
+
+        const response = await axios.post(
+          "http://localhost/BDD-MedicalEquipment/controller/tickets/createTicket.php",
+          formData,
+          { headers: { "Content-Type": "multipart/form-data" } }
+        );
+
+        console.log("Tarea creada:", response.data);
+        alert("Tarea creada con éxito");
+
+        // Limpiar formulario ya que no vamos a cerrar el modal automaticamente.
+        this.nuevaTarea = { inventario_id: "", descripcion: "", usuario_id: "" };
+      } catch (error) {
+        console.error("Error al guardar tarea:", error);
+      }
+    }
+  }
+};
+</script>
+
   
   <style scoped>
 .modal {
